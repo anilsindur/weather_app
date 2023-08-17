5814c70ea961bc1cc40cd4a696320accf2ea42b3
Anil SIndur <anilsindur7@gmail.com>
2023-08-17 10:30:29 +0530

    first commit

---
+++ src/components/weather/temp.js
@@ -0,0 +1,18 @@
+// https://api.openweathermap.org/data/2.5/weather?q=pune&appid=03d06b8610d566e1179976bfd9a07a13
+
+import React from 'react'
+
+const temp = () => {
+  return (
+    <>
+    <div className="wrap">
+         <div className="search">
+            
+         </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default temp
